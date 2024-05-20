2fe59527a5e92c7cb48a11d3f5e6e9112d4307d0
VictorOkpare <victorokpare@gmail.com>
2024-05-20 14:08:58 +0100

    changed the close menu

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -1,40 +1,85 @@
+// import './Header.css'
+// import { GiHamburgerMenu } from "react-icons/gi";
+
+  
+// import React, { useState } from 'react';
+
+//   const Header = () => {
+//     const [menuOpen, setMenuOpen] = useState(false);
+  
+//     function getMenuStyles(menuOpen){
+//       if(document.documentElement.clientWidth <= 800)
+//         return{
+//             right: !menuOpen && "-100%"
+//         }
+//     }
+  
+//     return (
+//       <div className="h-wrapper">
+//         <div className="h-container">
+//           <div className="h-logo">QuickLift</div>
+//           <div className="h-nav" style={getMenuStyles(menuOpen)}>
+//             <span><a href="#hero-wrapper">Home</a></span>
+//             <span><a href="#a-wrapper">About</a> </span>
+//             <span ><a href="#f-wrapper">Contact</a></span>
+//             <span >
+//              <a href="https://play.google.com/"> <button className='downloadbtn'>
+//               Download           
+//               </button></a> 
+//               </span>
+
+//           </div>
+//           <button className="hamburger" onClick={() => setMenuOpen(((m) => !m))}>
+//               <span className="hamburger-icon"><GiHamburgerMenu /></span>
+//             </button>
+//         </div>
+//       </div>
+//     );
+//   };
+  
+//   export default Header;
+
+
 import './Header.css'
 import { GiHamburgerMenu } from "react-icons/gi";
+import { MdClose } from "react-icons/md"; // Add this import
 
-  
 import React, { useState } from 'react';
 
-  const Header = () => {
-    const [menuOpen, setMenuOpen] = useState(false);
-  
-    function getMenuStyles(menuOpen){
-      if(document.documentElement.clientWidth <= 800)
-        return{
-            right: !menuOpen && "-100%"
-        }
-    }
-  
-    return (
-      <div className="h-wrapper">
-        <div className="h-container">
-          <div className="h-logo">QuickLift</div>
-          <div className="h-nav" style={getMenuStyles(menuOpen)}>
-            <span><a href="#hero-wrapper">Home</a></span>
-            <span><a href="#a-wrapper">About</a> </span>
-            <span ><a href="#f-wrapper">Contact</a></span>
-            <span >
-             <a href="https://play.google.com/"> <button className='downloadbtn'>
-              Download           
-              </button></a> 
-              </span>
+const Header = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
 
-          </div>
-          <button className="hamburger" onClick={() => setMenuOpen(((m) => !m))}>
-              <span className="hamburger-icon"><GiHamburgerMenu /></span>
-            </button>
+  function getMenuStyles(menuOpen){
+    if(document.documentElement.clientWidth <= 800)
+      return{
+          right: !menuOpen && "-100%"
+      }
+  }
+
+  return (
+    <div className="h-wrapper">
+      <div className="h-container">
+        <div className="h-logo">QuickLift</div>
+        <div className="h-nav" style={getMenuStyles(menuOpen)}>
+          <span><a href="#hero-wrapper">Home</a></span>
+          <span><a href="#a-wrapper">About</a> </span>
+          <span ><a href="#f-wrapper">Contact</a></span>
+          <span >
+           <a href="https://play.google.com/"> <button className='downloadbtn'>
+            Download           
+            </button></a> 
+          </span>
         </div>
+        <button className="hamburger" onClick={() => setMenuOpen(((m) => !m))}>
+          {menuOpen ? (
+            <MdClose className="hamburger-icon" /> // Render MdClose icon when menu is open
+          ) : (
+            <GiHamburgerMenu className="hamburger-icon" /> // Render GiHamburgerMenu icon when menu is closed
+          )}
+        </button>
       </div>
-    );
-  };
-  
-  export default Header;
+    </div>
+  );
+};
+
+export default Header;
